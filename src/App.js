8527083bdb8e0ac7e8d95db764ede5505bb4dfce
Ashita-gup <113427088+Ashita-gup@users.noreply.github.com>
2024-07-17 17:09:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+import './App.css';
+import Alert from './components/Alert';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
+function App() {
+  const[mode, setMode] = useState('light');
+  const[alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#0a1641';
+      showAlert("Dark mode has been enabled", "success");
+      document.title='TextUtils - Dark Mode ';
+      // setInterval(() => {
+        
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title='Install Textutils Now';
+      // }, 1500);
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title='TextUtils - Light Mode';
+    }
+  }
+  return (
+    <>
+    {/*<Router>*/}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert} />
+    <div className="container my-3">
+    {/*<Routes>*/}
+    {/*<Route exact path="/about" element={<About/>} >*/}
+    {/*</Route>*/}
+    {/*<Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your text to Analyse below" mode={mode}/>}>*/}
+    <TextForm showAlert={showAlert} heading="Enter your text to Analyse below" mode={mode}/>
+    {/*</Route>*/}
+    {/*</Routes>*/}
+    </div>
+    {/*</Router>*/}
+    
+   
+    </>
+  );
+}
+
+export default App;
